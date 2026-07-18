160e6414c4062f7da8a5b0201faf750a7c62e543
Sarkis Demirdjian <sarkdemi@gmail.com>
2026-07-18 11:36:31 +0300

    add git to help

--- Sercho's Terminal/Commands/Help.cs
+++ Sercho's Terminal/Commands/Help.cs
@@ -6,6 +6,7 @@ internal class Help
     {
         Console.WriteLine("'date' - Show current date");
         Console.WriteLine("'time' - Show current time");
+        Console.WriteLine("'git' - Use all git commands");
         Console.WriteLine("'dir' - Lists files and folders");
         Console.WriteLine("'mkdir' - Creates a folder");
         Console.WriteLine("'cd' - Changes directory");
